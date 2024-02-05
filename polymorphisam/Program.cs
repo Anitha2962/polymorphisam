0f12ac51bd3d654687bbe1cb54e853a5f9c78660
anitha2962 <anithasivadas92@gmail.com>
2024-02-05 17:28:56 +0530

    'first-commit'

---
+++ polymorphisam/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Addition
+{
+    public int Add(int a, int b)
+    {
+return a + b;
+    }
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+    public string Add(String a, String b)
+    {
+        return a + b;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Addition addition = new Addition();
+
+       int resultint = addition.Add(1, 2);
+        Console.WriteLine(resultint);
+        double resultdouble = addition.Add(1.77, 2.0);
+        Console.WriteLine(resultdouble);
+        string resultstring = addition.Add("hi", "hello");
+        Console.WriteLine(resultstring);
+    }
+}
